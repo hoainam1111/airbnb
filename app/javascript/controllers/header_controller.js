5a9fa2a2a062c2cd48474e1b53dfc7391fa9d0e6
hoainam1111 <namtre0712@gmail.com>
2024-10-29 19:35:33 +0000

    Devise redesign with tailwindUI templates

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+import {enter, leave, toggle} from 'el-transition'
+export default class extends Controller{
+    static targets = ['dropdown','openUserMenu']
+    connect(){
+        this.openUserMenuTarget.addEventListener("click", (e) => {
+            openDropdown(this.dropdownTarget)
+        })
+    }
+}
+function openDropdown(element) {
+    toggle(element).then(() => {
+        console.log("done")
+    })
+}
+
+function closeDropdown(){
+    leave(this.dropdownTarget).then(() => {
+        element.destroy();
+    })
+}
