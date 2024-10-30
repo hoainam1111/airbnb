3c40b906cd667dec50a4da750aec52d14969fbed
hoainam1111 <namtre0712@gmail.com>
2024-10-30 00:08:01 +0000

    use gem money rails to...with price, active_storage and show list properties with swiperjs

---
+++ app/models/property.rb
@@ -0,0 +1,13 @@
+class Property < ApplicationRecord
+  validates :name, presence: true
+  validates :headline, presence: true
+  validates :description, presence: true
+  validates :address_1, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :country, presence: true
+
+
+  monetize :price_cents, allow_nil: true
+  has_many_attached :images
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+6.times do |i|
+  property = Property.create!({
+    name: Faker::Lorem.unique.sentence(word_count: 3),
+    description: Faker::Lorem.paragraph(sentence_count: 10),
+    headline: Faker::Lorem.unique.sentence(word_count: 6),
+    address_1: Faker::Address.street_address,
+    address_2: Faker::Address.street_name,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    country: Faker::Address.country,
+    price: Money.from_amount(40, "USD")
+  })
+
+  property.images.attach(io: File.open("db/images/#{i+1}.jpg"), filename: property.name)
+  property.images.attach(io: File.open("db/images/#{i+7}.jpg"), filename: property.name)
+end
