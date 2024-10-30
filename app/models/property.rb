5f6d13f5b97eec66f60990852b24df8f43b4c8bc
hoainam1111 <namtre0712@gmail.com>
2024-10-30 18:09:31 +0000

    average rating and design wishlish with stimulus

--- app/models/property.rb
+++ app/models/property.rb
@@ -11,4 +11,10 @@ class Property < ApplicationRecord
   monetize :price_cents, allow_nil: true
   has_many_attached :images
   has_many :reviews, dependent: :destroy
+
+  def update_average_rating
+    # this will get called whenever a new review is added or updated.
+    average_rating = reviews.average(:final_rating)
+    update_column(:average_final_rating, average_rating)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
-  belongs_to :property
+  belongs_to :property, counter_cache: true
 
   validates :content, presence: true
   validates :cleanliness_rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
@@ -16,5 +16,6 @@ class Review < ApplicationRecord
     total_points = (cleanliness_rating + accuracy_rating + checkin_rating + communication_rating + location_rating + value_rating) / 6
 
     update_column(:final_rating, total_points.to_f)
+    property.update_average_rating
   end
 end
